1df3770b22fd0664b615f7c0ddcf1cc1606c2caf
Valentin Magaña <valentin.magana@adweb.mx>
2017-08-10 15:26:37 -0500

    Implementación de campos de cotización (renta)
    Versión estable
    
    -Implementación de modulo en el sale_order_line.py
    -Ajustes en las vistas del modulo order_line y la vista para la
    cotización.

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -3,7 +3,65 @@
 
 from odoo import api, fields, models
 
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        for order in self:
+            amount_untaxed = amount_tax = amount_days_retail = 0.0
+            if order.ae_tipo_cotizacion == 'cotizacion_para_renta':
+                if order.ae_renta_dia <= 0:
+                    order.ae_renta_dia = 1
+                for line in order.order_line:
+                    amount_untaxed += (line.price_subtotal * order.ae_renta_dia)
+                    amount_tax += (line.price_tax * order.ae_renta_dia)
+                    amount_days_retail = order.ae_renta_dia
+            else:
+                for line in order.order_line:
+                    amount_untaxed += line.price_subtotal
+                    amount_tax += line.price_tax
+                    amount_days_retail = 1
+            order.update({
+                'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
+                'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+                'amount_days_retail':  order.pricelist_id.currency_id.round(amount_days_retail),
+                'amount_total': (amount_untaxed + amount_tax),
+            })
+
+    ae_tipo_cotizacion = fields.Selection([('cotizacion_para_venta', 'Cotización para Venta'), ('cotizacion_para_renta', 'Cotización para Renta')], string='Tipo de Cotización',
+                                     readonly=True,states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+                                     default='cotizacion_para_venta')
+    ae_renta_dia = fields.Integer(string='Días de Renta', default=1, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
+                                     track_visibility='always')
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
+                                 track_visibility='always')
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
+                                   track_visibility='always')
+    amount_days_retail = fields.Integer(string='Días de Renta', store=True, readonly=True, compute='_amount_all',
+                                   track_visibility='always')
+
+    @api.onchange('ae_tipo_cotizacion', 'ae_renta_dia', 'order_line')
+    def supply_rate(self):
+        for order in self:
+            order.amount_untaxed = order.amount_tax = order.amount_total = 0
+            if order.ae_tipo_cotizacion == 'cotizacion_para_renta':
+                if order.ae_renta_dia <= 0:
+                    order.ae_renta_dia = 1
+                for line in order.order_line:
+                    order.amount_untaxed += (line.price_subtotal * order.ae_renta_dia)
+                    order.amount_tax += (line.price_tax * order.ae_renta_dia)
+                    order.amount_days_retail = order.ae_renta_dia
+            else:
+                for line in order.order_line:
+                    order.amount_untaxed += line.price_subtotal
+                    order.amount_tax += line.price_tax
+                    order.amount_days_retail = 1
+            order.amount_total = order.amount_untaxed + order.amount_tax
+
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    ae_dias_renta_line = fields.Integer(string='Días', default=1)
+    ae_dias_renta_line = fields.Integer(string='Días', default=3)
