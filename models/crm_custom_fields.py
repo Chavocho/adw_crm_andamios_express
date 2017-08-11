61276bb24c105b54af83bcc074934b581cc14447
Valentin Magaña <valentin.magana@adweb.mx>
2017-08-11 18:46:21 -0500

    Cambios a PDF, Campo adicional
    -Se añadió un nuevo campo a la vista de Oportunidad.
    -Se hicieron cambios al PDF.

--- models/crm_custom_fields.py
+++ models/crm_custom_fields.py
@@ -7,9 +7,6 @@ class CrmCustomFields(models.Model):
 
   _inherit = "crm.lead"
 
-  ae_metodo_pago = fields.Selection(
-      [('efectivo','Efectivo'),('cheque', 'Cheque'),('transferencia', 'Transferencia'),('tarjeta_de_debito', 'Tarjeta de Débito'),('tarjeta_de_credito', 'Tarjeta de Crédito'),('deposito_efectivo', 'Deposito en Efectivo'),('otros', 'Otros')],
-      'Servicio', help="Método de Pago")
-  ae_servicio = fields.Selection(
-      [('andamiaje_y_accesorios','Andamiaje y Accesorios'),('puntal_y_cimbra', 'Puntal y Cimbra'),('servicios', 'Servicios')],
-      'Servicio', help="Servicio Solicitado")
+  ae_metodo_pago = fields.Selection( [('efectivo','Efectivo'),('cheque', 'Cheque'),('transferencia', 'Transferencia'),('tarjeta_de_debito', 'Tarjeta de Débito'),('tarjeta_de_credito', 'Tarjeta de Crédito'),('deposito_efectivo', 'Deposito en Efectivo'),('otros', 'Otros')], 'Método de Pago', help="Método de Pago")
+  ae_servicio = fields.Selection( [('andamiaje_y_accesorios','Andamiaje y Accesorios'),('puntal_y_cimbra', 'Puntal y Cimbra'),('servicios', 'Servicios')], 'Servicio Solicitado', help="Servicio Solicitado")
+  ae_tipo_cotizacion = fields.Selection([('venta','Venta'),('renta', 'Renta')], 'Tipo de Cotización', help="Tipo de Cotización")
--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -9,22 +9,25 @@ class SaleOrder(models.Model):
     @api.depends('order_line.price_total')
     def _amount_all(self):
         for order in self:
-            amount_untaxed = amount_tax = amount_days_retail = 0.0
+            amount_untaxed = amount_tax = amount_days_retail = rent_amount_untaxed = 0.0
             if order.ae_tipo_cotizacion == 'cotizacion_para_renta':
                 if order.ae_renta_dia <= 0:
                     order.ae_renta_dia = 1
                 for line in order.order_line:
                     amount_untaxed += (line.price_subtotal * order.ae_renta_dia)
                     amount_tax += (line.price_tax * order.ae_renta_dia)
+                    rent_amount_untaxed += line.price_subtotal
                     amount_days_retail = order.ae_renta_dia
             else:
                 for line in order.order_line:
                     amount_untaxed += line.price_subtotal
                     amount_tax += line.price_tax
+                    rent_amount_untaxed = 0
                     amount_days_retail = 1
             order.update({
                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+                'rent_amount_untaxed': order.pricelist_id.currency_id.round(rent_amount_untaxed),
                 'amount_days_retail':  order.pricelist_id.currency_id.round(amount_days_retail),
                 'amount_total': (amount_untaxed + amount_tax),
             })
@@ -36,6 +39,8 @@ class SaleOrder(models.Model):
 
     amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all',
                                      track_visibility='always')
+    rent_amount_untaxed = fields.Monetary(string='Untaxed Amount Rent', store=True, readonly=True, compute='_amount_all',
+                                     track_visibility='always')
     amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all',
                                  track_visibility='always')
     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all',
@@ -46,18 +51,20 @@ class SaleOrder(models.Model):
     @api.onchange('ae_tipo_cotizacion', 'ae_renta_dia', 'order_line')
     def supply_rate(self):
         for order in self:
-            order.amount_untaxed = order.amount_tax = order.amount_total = 0
+            order.amount_untaxed = order.amount_tax = order.amount_total = order.rent_amount_untaxed = 0.0
             if order.ae_tipo_cotizacion == 'cotizacion_para_renta':
                 if order.ae_renta_dia <= 0:
                     order.ae_renta_dia = 1
                 for line in order.order_line:
                     order.amount_untaxed += (line.price_subtotal * order.ae_renta_dia)
                     order.amount_tax += (line.price_tax * order.ae_renta_dia)
+                    order.rent_amount_untaxed += line.price_subtotal
                     order.amount_days_retail = order.ae_renta_dia
             else:
                 for line in order.order_line:
                     order.amount_untaxed += line.price_subtotal
                     order.amount_tax += line.price_tax
+                    order.rent_amount_untaxed = 0
                     order.amount_days_retail = 1
             order.amount_total = order.amount_untaxed + order.amount_tax
 
